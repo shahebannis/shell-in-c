11943d71492b1ca7d265c6e0bc7c79edf9fc0ef5
Shahe Bannis <74209018+shahebannis@users.noreply.github.com>
2026-09-09 17:18:10 -0400

    Rudimentary shell

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Shell in C
+
+It is currently able to handle output redirects, run commands, run a script with comments, use a shebang
---
+++ main.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int handle_redirect(char *args[]);
+
+int main(int argc, char *argv[]) {
+    int interactive = 1;
+
+    // for scripting
+    if (argc > 1) {
+        //first arg is the name of the script
+        if (freopen(argv[1], "r", stdin) == NULL) {
+            fprintf(stderr, "Can't read from script file %s\n", argv[1]);
+            fprintf(stderr, "Exiting.\n");
+            exit(1);
+        }
+        interactive = 0;
+    }
+
+
+    while (1) {
+        //string to hold input buffer
+        char buf[1024];
+
+        // Prompt
+        if (interactive)
+            printf("$ ");
+
+        //get input from keyboard
+        if (fgets(buf, 1024, stdin) == NULL) break; // added the null check because without it it would print \n forever if scripting
+
+        // trim newline
+        char *nl = strchr(buf, '\n'); //gives a pointer to the newline
+        if (nl) *nl = '\0'; //replaces that pointer's value with '\0' if found
+                            
+        //remove comments (also allows for shebangs to work because of how execvp works and then comment removal remove the shebang right before execution)
+        char *hash = strchr(buf, '#');
+        if (hash) *hash = '\0';
+    
+
+
+        // split buf into args
+        char *args[20]; // watch out for overflowing buffer if im to continue this project
+        int nargs = 0;
+
+        args[nargs] = strtok(buf, " ");
+        while (args[nargs] != NULL) {
+            args[++nargs] = strtok(NULL, " "); // NULL here due to the way strtok works. First instance needs the string, subsequent using the same input need to be passed null. See man strtok
+        }
+
+        if (args[0] == NULL) continue;
+        if (strcmp(args[0], "exit") == 0) exit(0); //when running a command that does not exist right before, you need to exit twice. Probably due to the fork still running or something, idk.
+        //fork and execute
+        pid_t pid = fork();
+
+        if (pid > 0) {
+            // parent
+            wait(NULL);
+        }
+        else {
+            // child
+            //
+            if (handle_redirect(args) == -1) {
+                fprintf(stderr, "Could not redirect\n");
+                exit(1);
+            }
+
+            execvp(args[0], args);
+
+            fprintf(stderr, "Could not exec %s\n", buf);
+        }
+    }
+    return 0;
+}
+
+
+// stdin same but < and open in read and replace stdin
+// Handle redirect stdout
+// Return 1 if succ
+// Return 0 if no redirect necesasry
+// Return -1 if something broke
+int handle_redirect(char *args[]) {
+    for (int i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], ">") == 0) {
+            //assuming next arg is always the file to write to
+            if ( freopen(args[i+1], "w", stdout) == NULL ) return -1; //reopen to replace stdout file descriptor with this new file. Also replaces abstraction layer on top out stdout
+                                                                      //or use open but a lot more work, but probably better
+
+            // Terminate args at the ">" symbol
+            args[i] = NULL;
+            return 1;
+        }
+    }
+    return 0;
+}
